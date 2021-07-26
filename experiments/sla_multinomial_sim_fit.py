ccc89bda6cf4cec654a421c854c2a65921524340
zihao12 <wzihao12@gmail.com>
2021-07-25 22:22:43 -0500

    update sla_multinomial1.ipynb

---
+++ experiments/sla_multinomial_sim_fit.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import pickle
+import argparse
+from scipy import sparse
+import numpy as np
+
+script_dir = "../"
+sys.path.append(os.path.abspath(script_dir))
+from file2 import *
+from factorize import *
+from smallsim_functions import simulate_multinomial_counts
+from misc import read_fitted_rds
+
+
+parser = argparse.ArgumentParser(description='sla_multinomial_sim_fit')
+parser.add_argument('-d', type=str)
+parser.add_argument('-o', type=str)
+parser.add_argument('-r', type=float)
+
+args = parser.parse_args()
+
+datafile=args.d
+outputfile=args.o
+rate=args.r
+
+
+print(datafile)
+print(outputfile)
+print(rate)
+
+np.random.seed(123)
+
+# rate=1
+# datafile="../../ebpmf_data_analysis/output/fastTopics_fit/fit_sla_fastTopics_k6.Rds"
+# outputfile="output/fit_sim_sla_fastTopics_k6_rate1.pkl"
+
+print("load model")
+fitted = read_fitted_rds(datafile)
+F = fitted['F']
+L = fitted['L']
+s = rate*fitted['s'].astype(int)
+
+print("simulate data")
+X,w_idct, F = simulate_multinomial_counts(L, F, s)
+X = sparse.coo_matrix(X)
+
+print("get Ctrue, Atrue")
+n, p = X.shape
+k = F.shape[1]
+Ctrue = F @ L.T
+Ctrue = Ctrue @ Ctrue.T
+Ctrue /= Ctrue.sum()
+Atrue = (L.T @ L) / n
+
+print("fit on data")
+C, _, _ = X2C(X)
+S, B, A, _, _, _, _, C = factorizeC(C, K=k, rectifier='AP', optimizer='activeSet')
+
+print("get S0 from Ctrue")
+S0, B0, A0, _, _, _, _, _ = factorizeC(Ctrue, K=k, rectifier='no', optimizer='activeSet')
+
+
+
+out = {'X':X, 'Ftrue':F, 'Ltrue':L, 'Atrue':Atrue, 
+'C':C, 'S':S, 'B':B, 'A':A, 'S0':S0, 'B0':B0, 'A0':A0}
+
+file = open(outputfile, 'wb')
+pickle.dump(out, file)
+file.close()
---
+++ experiments/sla_multinomial_sim_fit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_FIT=sla_multinomial_sim_fit.py
+dataname=droplet
+k=6
+inputname=../../ebpmf_data_analysis/output/fastTopics_fit/fit_${dataname}_fastTopics_k${k}.Rds
+
+for rate in 3 1 0.5
+do 
+	outputname=output/fit_sim_${dataname}_fastTopics_k${k}_rate${rate}.pkl
+	python ${SCRIPT_FIT} -d ${inputname} -o ${outputname} -r ${rate} > ${outputname}.out
+done
