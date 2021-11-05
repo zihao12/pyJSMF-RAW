729a7151ca9630b502433e4e279f1450f4128142
Zihao Wang <wzihao12@gmail.com>
2021-11-05 09:28:39 -0500

    update misc

--- misc.py
+++ misc.py
@@ -2,6 +2,9 @@ import numpy as np
 import rpy2.robjects as robjects
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from scipy.stats import betabinom
+from scipy.optimize import minimize
+
 
 
 
@@ -32,7 +35,7 @@ def match_topics(F1, F2):
 
 
 ## visualize the extremal rows of matrix X (intended for when X is a lower dimension embedding)
-def vis_extremal_pca(X, S, which_dim = [0, 1]):
+def vis_extremal_pca(X, S, which_dim = [0, 1], annotate=False,fontsize=6):
 	mask = np.zeros(X.shape[0])
 	mask[S] = 1
 	mask = mask.astype(bool)
@@ -45,8 +48,14 @@ def vis_extremal_pca(X, S, which_dim = [0, 1]):
 	ax1.scatter(X[mask,which_dim[0]], 
 	            X[mask, which_dim[1]], 
 	            s=30, c='r', marker="o", label='second')
+	if annotate:
+		for s in S:
+			ax1.annotate(s, (X[s,which_dim[0]], X[s,which_dim[1]]), 
+				fontsize = fontsize, rotation=45)
 	# plt.legend(loc='upper left');
 	plt.show()
+
+
 
 def Cbar_proj(C):
 	C = C/C.sum(axis=1)[:,None]
@@ -54,3 +63,34 @@ def Cbar_proj(C):
 	pca.fit(C)
 	
 	return pca.transform(C)
+
+def pca_proj(X):
+	pca = PCA(n_components=10)
+	
+	
+	return pca.fit_transform(X)
+
+def betabinom_shrinkage(n, x, ab = np.array([1,1])):
+    ab = mle_bb(n, x, ab)
+    mu, var =  beta_mean_var(ab[0], ab[1])
+    
+    return pos_beta(n, x, ab), ab, mu, var
+
+def obj_bb(ab_root, n, x):
+    ab = ab_root**2
+    return -betabinom.logpmf(x, n, ab[0], ab[1], loc=0).sum()
+
+def mle_bb(n, x, ab):
+    res = minimize(obj_bb, np.sqrt(ab), args=(n, x), method='BFGS')
+    
+    return res.x**2
+
+def pos_beta(n, x, ab):
+    
+    return (ab[0] + x) / (ab.sum() + n)
+
+def beta_mean_var(a, b):
+    mu = a / (a + b)
+    var = a*b/((a + b + 1) * (a+b)**2)
+    
+    return mu, var
