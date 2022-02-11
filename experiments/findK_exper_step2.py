faf70308fdb1989c80a788b86e33989a72f56c9b
Zihao Wang <wzihao12@gmail.com>
2022-02-11 12:26:47 -0600

    add more experiments

---
+++ experiments/findK_exper_step2.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import pickle
+import argparse
+from scipy import sparse, io 
+import numpy as np
+import rpy2.robjects as robjects
+import magic
+import scprep
+from sklearn.decomposition import NMF, non_negative_factorization, PCA
+from sklearn.decomposition._nmf import _beta_divergence
+import seaborn as sns
+import pandas as pd
+from scipy.sparse import csr_matrix, csc_matrix 
+
+
+script_dir = "../"
+sys.path.append(os.path.abspath(script_dir))
+from file2 import *
+from factorize import *
+from smallsim_functions import simulate_multinomial_counts
+from misc import *
+from findK_correction import *
+
+import pdb
+
+np.random.seed(123)
+
+
+def simulate_multinomial_counts_local(L, F, s):
+    n = L.shape[0]
+    p = F.shape[0]
+    Prob = L.dot(F.T)
+    X = np.empty(shape = (n, p))
+    for i in range(n):
+        X[i,] = np.random.multinomial(s[i], Prob[i,], size = 1).astype(int)
+
+    return X
+
+datafile="../../ebpmf_data_analysis/output/fastTopics_fit/sla_small_fitted.Rds"
+Yfile = "../../ebpmf_data_analysis/output/fastTopics_fit/sla_small.txt"
+vocabfile="../../ebpmf_data_analysis/output/fastTopics_fit/sla_small_vocab.txt"
+titlefile="../../ebpmf_data_analysis/data/SLA/title.sla.txt"
+
+# Y = io.mmread(Yfile)
+vocab = np.loadtxt(vocabfile, dtype = str)
+readRDS = robjects.r['readRDS']
+data = readRDS(datafile)
+
+w_idx = np.asarray(data.rx2('word_idx')).astype(int)
+fitted = data.rx2('fit_sub')
+F = np.asarray(fitted.rx2('F'))
+L = np.asarray(fitted.rx2('L'))
+s = np.asarray(fitted.rx2('s'))
+
+p, k = F.shape
+n = L.shape[0]
+C = L @ F.T
+C = C.T @ C / n
+Cbar = C / C.sum(axis = 1)[:, None]
+S0, diagR0, _ = findS(Cbar,k)
+
+
+n_sample = 1000
+est = np.zeros((p, n_sample))
+est_notcorrected = np.zeros((p, n_sample))
+
+d = s
+const = d 
+const = 1 / (const * (const -1)).sum()
+
+u = Cbar[S0[0], :]
+u /= np.sqrt((u**2).sum())
+truth = (Cbar**2).sum(axis = 1) - (Cbar @ u)**2
+
+for i in range(n_sample):
+    X = simulate_multinomial_counts_local(L, F, d)
+    if X.sum(axis = 0).min() < 1:
+        pass
+    C_ = compute_C_unbiased_local(X)
+    Cbar_ = C_ / C_.sum(axis = 1)[:, None]
+    weights = C_.sum(axis = 1)
+    est[:, i] = Cbar_pj_rss(C_, u, weights, const)
+    est_notcorrected[:, i] = (Cbar_**2).sum(axis = 1) - (Cbar_ @ u)**2
+    
+outputfile="findK-exper-step2.pkl"
+with open(outputfile, "wb") as f:
+    pickle.dump({'est':est, 'est_notcorrected':est_notcorrected, 'truth':truth,
+        'C':C, 'S0':S0}, f)
---
+++ findK_correction.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def compute_C_unbiased_local(X):
+    ## X is n by p
+    n,  p = X.shape
+    d = X.sum(axis = 1)
+    m = (d * (d -1)).sum()
+    C = (X.T @ X) - np.diag(X.sum(axis = 0))
+    C = C / m
+    
+    return C
+
+def findS_correction(Cbar, C, K, const):
+    R = Cbar.T.copy()## why? I change Cbar without .copy()
+    S = np.zeros(K)
+    u = None
+    colSquaredSums = (R**2).sum(axis = 0)
+    diagR = np.zeros(K)
+
+    for k in range(K):
+        colSquaredSums -= correction(C, u, const)
+        maxSquaredSum = np.max(colSquaredSums)
+        maxCol = np.argmax(colSquaredSums)
+        S[k] = maxCol
+        u = R[:, maxCol]
+        u = u / np.sqrt(maxSquaredSum)
+        diagR[k] = np.sqrt(maxSquaredSum)
+        colSquaredSums -= (u.T @ R)**2
+        
+        R = R -  u[:, None] @ (u[:,None].T @ R)
+        
+    return S, diagR
+
+def Cbar_pj_rss(C, u, weights, const):
+    C_ = C.copy()
+    Cbar = C_ / C_.sum(axis = 1)[:, None]
+    out = (Cbar**2).sum(axis = 1) - (Cbar @ u)**2
+    out -= correction(C_, None, const)
+    out -= correction(C_, u, const)
+    
+    return out
+
+
+def correction(C, u, const):
+    weights = C.sum(axis = 1)
+    if u is None:
+        return const / weights
+    return - const * (C @ (u**2)) / (weights**2) - const * (C @ u)**2 / (weights**2)
+
+
+
+def Cbar_rss(Cbar, weights, const):
+    weights_inv = 1/weights
+    rss_est = compute_rss_mean(Cbar, const, weights_inv)
+    rsq_est = compute_rsq_mean(Cbar, rss_est, const, weights_inv)
+    rss_var = compute_rss_var(rss_est, rsq_est, const, weights_inv)
+    
+    return rss_est, rss_var
+    
+def compute_rss_var(rss_est, rsq_est, const, weights_inv):
+    rss_var = (const**3) * (weights_inv**3) + 6 * (const**2) * (weights_inv**2) * rss_est 
+    rss_var += 4 * const * weights_inv * rsq_est
+    
+    return rss_var
+
+def compute_rss_mean(Cbar, const, weights_inv):
+    rss_est = (Cbar**2).sum(axis = 1) - const * weights_inv
+    
+    return rss_est
+
+def compute_rsq_mean(Cbar, rss_est, const, weights_inv):
+    rsq_est = (Cbar**3).sum(axis = 1) - 3 * const * weights_inv * rss_est - (const*weights_inv)**2
+    
+    return rsq_est
+
+def simulate_multinomial_counts_local(L, F, s):
+    n = L.shape[0]
+    p = F.shape[0]
+    Prob = L.dot(F.T)
+    X = np.empty(shape = (n, p))
+    for i in range(n):
+        X[i,] = np.random.multinomial(s[i], Prob[i,], size = 1).astype(int)
+
+    return X
